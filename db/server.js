9bc9f3854979c4e9dfaa91b85b88ee17832ebee9
Harsimran <harsimranbhullar01@gmail.com>
2025-07-23 14:44:57 -0700

    Routing issues

--- db/server.js
+++ db/server.js
@@ -1,17 +1,11 @@
 import express from "express";
 import pokemonRoute from "./routes/pokemon.js";
 import dotenv from 'dotenv';
-import cors from "cors";
 
 dotenv.config();
 const app = express(); // ✅ define app before using it
 app.use(express.json());
 
-
-app.use(cors({
-  origin: 'https://game-management-mauve.vercel.app',
-  credentials: true
-}));
 
 app.use("/api/pokemon", pokemonRoute);
 
